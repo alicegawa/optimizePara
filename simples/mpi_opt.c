924da76210238e9a6e5fb0868f0b2c0708ad0743
alicegawa <fukuda@brain.imi.i.u-tokyo.ac.jp>
2016-01-14 17:44:44 +0900

    simples内で，split後のCOLORに応じて，生成ファイルの名前が競合しないようにしたものを追加．
    -cオプションを持たせることにより可能．

--- simples/mpi_opt.c
+++ simples/mpi_opt.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv){
     char specials[]="./special";
     char **option;
     char *neuron_argv[] = {"-mpi", "-nobanner", "para_test.hoc", NULL};
+    char **neuron_argv2;
     char option_mpi[] ="-mpi", HOCFILE[] = "para_test.hoc", option_nobanner[]="-nobanner";
     int maxprocs=2;
     
@@ -31,6 +32,21 @@ int main(int argc, char **argv){
 	option[i] = (char *)malloc(sizeof(char) * 10);
     }    
 
+    neuron_argv2 = (char **)malloc(sizeof(char *) * 6);
+    for(i=0;i<6;i++){
+	neuron_argv2[i] = (char *)malloc(sizeof(char) * 10);
+	if(i==0){
+	    sprintf(neuron_argv2[i],"%s",option_mpi);
+	}else if(i==1){
+	    sprintf(neuron_argv2[i],"%s",option_nobanner);
+	}else if(i==2){
+	    sprintf(neuron_argv2[i],"-c");
+	}else if(i==4){
+	    sprintf(neuron_argv2[i],"%s",HOCFILE);
+	}else{
+	}
+    }
+    
     if(argc>1){
 	num_memofgroup = atoi(argv[1]);
     }
@@ -40,37 +56,41 @@ int main(int argc, char **argv){
     }
         
     key = 0;
-    color = myid%num_memofgroup;
+    color = myid/num_memofgroup;
     call_final = numprocs/num_memofgroup;
 
     MPI_Comm_split(MPI_COMM_WORLD, color, key, &comm1);
     MPI_Comm_rank(comm1, &new_myid);
     MPI_Comm_size(comm1, &new_size);
+
+    sprintf(neuron_argv2[3],"COLOR=%d",color);
+    neuron_argv2[5] = NULL;
     
-    if(new_myid==0){
-	sprintf(filename,"data%d.dat",color+1);
+    /* if(new_myid==0){ */
+    /* 	sprintf(filename,"data%d.dat",color+1); */
 	
-	if((fp=fopen(filename,"r"))==NULL){
-	    printf("file cannot be open");
-	    return -1;
-	}
+    /* 	if((fp=fopen(filename,"r"))==NULL){ */
+    /* 	    printf("file cannot be open"); */
+    /* 	    return -1; */
+    /* 	} */
 
-	for(i=0;i<4;i++){
-	    fscanf(fp,"%d",&data[i]);
-	    sprintf(option[i],"%d",data[i]);
-	    //printf("option[%d] is %s\n",i, option[i]);
-	    //printf("root of each group color is %d, data[%d] = %d\n", color, i, data[i]);
-	}
+    /* 	for(i=0;i<4;i++){ */
+    /* 	    fscanf(fp,"%d",&data[i]); */
+    /* 	    sprintf(option[i],"%d",data[i]); */
+    /* 	    //printf("option[%d] is %s\n",i, option[i]); */
+    /* 	    //printf("root of each group color is %d, data[%d] = %d\n", color, i, data[i]); */
+    /* 	} */
 
-	fclose(fp);
-    }
+    /* 	fclose(fp); */
+    /* } */
 
     printf("test for comm spawn\n");
     //test for comm_spawn
     //printf("my original ID = %d, start the mpi comm spawn section\n",myid);
     //MPI_Comm_spawn(command, option, 4, MPI_INFO_NULL, 0, comm1, &comm2, MPI_ERRCODES_IGNORE);
     //printf("finish mpi spawn test for \"add\"\n");
-    MPI_Comm_spawn(specials, neuron_argv, 4, MPI_INFO_NULL, 0, comm1, &comm2, MPI_ERRCODES_IGNORE);
+    //MPI_Comm_spawn(specials, neuron_argv, 4, MPI_INFO_NULL, 0, comm1, &comm2, MPI_ERRCODES_IGNORE);
+    MPI_Comm_spawn(specials, neuron_argv2, 4, MPI_INFO_NULL, 0, comm1, &comm2, MPI_ERRCODES_IGNORE);
     MPI_Intercomm_merge(comm2,0, &nrn_comm);
     printf("finish comm spawn\n");
     fflush(stdout);

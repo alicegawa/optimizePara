6cf56931b1e7bc7433a52c4c4a11e36870c878c5
alicegawa <fukuda@brain.imi.i.u-tokyo.ac.jp>
2016-01-25 14:02:35 +0900

    ファイル入出力のテスト用プログラムの作成
    スパコンでは未テスト

--- simples/print_color_id.c
+++ simples/print_color_id.c
@@ -5,7 +5,9 @@
 int main(int argc, char **argv){
     int color;
     int numprocs, myid;
-
+    FILE *fp;
+    char filename[50];
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
@@ -16,8 +18,12 @@ int main(int argc, char **argv){
     }
 
     color = atoi(argv[1]);
+    sprintf(filename,"color_%d_id_%d.txt",color, myid);
+    fp = fopen(filename,"w");
+    
     printf("I am %d color\'s %d th process\n", color, myid);
-
+    fprintf(fp, "I a %d color\"s %d th process \n", color, myid);
+    fclose(fp);
     MPI_Finalize();
     return 0;
 }

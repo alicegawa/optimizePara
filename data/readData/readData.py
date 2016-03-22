214df88dc43d5ad7988a68f033b36a91f75f2804
alicegawa <fukuda@brain.imi.i.u-tokyo.ac.jp>
2016-03-22 17:52:16 +0900

    解析用プログラムで，単一ファイルの処理は可能になった．
    ファイル構成からさらって，一気に解析できるように書き換える．

--- data/readData/readData.py
+++ data/readData/readData.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('./data/991109_3_p_Bol_kj.dat', skiprows=1)
+filename_tmp = './data/991109_3_p_Bal_kj'
+filename = filename_tmp + '.dat'
+data = np.loadtxt(filename, skiprows=1)
 
 time = data[:,0]
 response =  data[:,1]
@@ -14,30 +16,87 @@ ave_v = np.average(response)
 
 threshold = 50
 
-def spike_detect(res, index):
+olf_dur = 0.5
+bin_width = 0.25
+
+def show_olf_strength(olf):
+    ave_olf = np.average(olf)
+    for i in range(0, len(olf)):
+        if olf[i] > ave_olf and olf[i-1]==olf[i] and olf[i]==olf[i+1]:
+                print '%f' %olf[i]
+                return olf[i]
+    print 'failure'
+    return -1
+
+olf_judge_strength = (show_olf_strength(olfactory) + np.average(olfactory)) * 0.5
+#olf_strength = -0.0083
+
+        
+def spike_detect(res, start, end):
     spike_counter = 0
-    for i in range(0, index):
+    for i in range(start, end):
         if res[i] > threshold:
             if res[i-1] < res[i] and res[i] > res[i+1]:
                 spike_counter = spike_counter + 1
     return spike_counter
 
-t_start_olf = 0
-t_start_olf_index = -1
+def get_index_of_time(time, target_time):
+    for i in range(0, len(time)):
+        if time[i] >= target_time:
+            return i
 
-for i in range(0, len(time)):
-    if time[i] >= 0:
-        t_start_olf = time[i]
-        t_start_olf_index = i
-        break
+def detect_olf_timing(olf, start):
+    for i in range(start, len(olf)):
+        if olf_judge_strength == -1:
+            return -1
+        else:
+            if olf[i] > olf_judge_strength:
+                return i
+    return -1
 
-print 't_start_olf_index = %d' % t_start_olf_index
+def calc_spike_and_frequence(time, res, olf, start_index, spike_counter_spon_tmp):
+    t_start_olf_index = detect_olf_timing(olf, start_index)
+    if t_start_olf_index != -1:
+        t_start_olf = time[t_start_olf_index]
+        t_end_olf_index = get_index_of_time(time, t_start_olf + olf_dur)
+        t_after_olf_part1 = get_index_of_time(time, t_start_olf + olf_dur + bin_width)
+        t_after_olf_part2 = get_index_of_time(time, t_start_olf + olf_dur + bin_width * 2)
+        t_after_olf_part3 = get_index_of_time(time, t_start_olf + olf_dur + bin_width * 3)
+        t_after_olf_part4 = get_index_of_time(time, t_start_olf + olf_dur + bin_width * 4)
+
+        if start_index == 0:
+            spike_counter_spontaneous = spike_detect(res, 0, t_start_olf_index)
+        else:
+            spike_counter_spontaneous = spike_counter_spon_tmp
+        spike_counter_part1 = spike_detect(res, t_end_olf_index, t_after_olf_part1)
+        spike_counter_part2 = spike_detect(res, t_after_olf_part1, t_after_olf_part2)
+        spike_counter_part3 = spike_detect(res, t_after_olf_part2, t_after_olf_part3)
+        spike_counter_part4 = spike_detect(res, t_after_olf_part3, t_after_olf_part4)
+
+        ave_f_spontaneous = spike_counter_spontaneous / (0 - time[0])
+        ave_f_part1 = spike_counter_part1 / bin_width
+        ave_f_part2 = spike_counter_part2 / bin_width
+        ave_f_part3 = spike_counter_part3 / bin_width
+        ave_f_part4 = spike_counter_part4 / bin_width
         
-spike_counter_spontaneous = spike_detect(response, t_start_olf_index)
-spike_counter_full = spike_detect(response, len(response))
-print 'num of spike till 0 points = %d' % spike_counter_spontaneous
-print 'num of spike of full time = %d'% spike_counter_full
+        #print 'firing rate: spontaneous\tsection1\tsection2\tsection3\tsection4\n'
+        print '%f\t%f\t%f\t%f\t%f\n' %(ave_f_spontaneous, ave_f_part1, ave_f_part2, ave_f_part3, ave_f_part4)
+        #print 'spike num: spontaneous\tsection1\tsection2\tsection3\tsection4\n'
+        print '%d\t%d\t%d\t%d\t%d\n' %(spike_counter_spontaneous, spike_counter_part1, spike_counter_part2, spike_counter_part3, spike_counter_part4)
+        ave_f_full = (ave_f_part1 + ave_f_part2 + ave_f_part3 + ave_f_part4) * 0.25
+        print 'firing rate of full section after 1(sec) of olfactory stimulus = %f\n' % ave_f_full
 
+        return (t_after_olf_part4, spike_counter_spontaneous)
+    else:
+        return (-1, spike_counter_spon_tmp)
+
+start_index = 0
+spike_counter_spon_tmp = 0
+while True:
+    start_index, spike_counter_spon_tmp = calc_spike_and_frequence(time, response, olfactory, start_index, spike_counter_spon_tmp)
+    if start_index == -1:
+        break
+        
 fig = plt.figure(figsize=(15,6))
 ax1 = fig.add_subplot(211)
 plt.xticks(fontsize=16)
@@ -51,11 +110,13 @@ ax1.axhline(y=max_v, color='red')
 ax1.axhline(y=50, color='yellow')
 ax1.axhline(y=min_v, color='green')
 ax1.axhline(y=ave_v, color='black')
+ax2.axhline(y=olf_judge_strength, color='black')
 ax1.set_title('time-response(upper) and time-olfactory(downer)', fontsize=20)
 ax1.set_ylabel('response [mV]', fontsize=18)
 ax2.set_xlabel('time [sec]', fontsize=18)
 ax2.set_ylabel('olfactory [V]', fontsize=18)
-#plt.savefig("./data/data.png")
+filename = filename_tmp + '.png'
+plt.savefig(filename)
 print 'max_v = %f' % max_v
 print 'min_v = %f' % min_v
 print 'ave_v = %f' % ave_v

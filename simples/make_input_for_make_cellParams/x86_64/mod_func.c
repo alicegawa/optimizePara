0c808a961c9dbecca9c9d778db15d1328934241b
alicegawa <fukuda@brain.imi.i.u-tokyo.ac.jp>
2016-11-10 18:29:56 +0900

    LAL入力細胞の実装を，hh_kでの実行用に変更．
    スパコン用？

--- simples/make_input_for_make_cellParams/x86_64/mod_func.c
+++ simples/make_input_for_make_cellParams/x86_64/mod_func.c
@@ -3,14 +3,17 @@
 extern int nrnmpi_myid;
 extern int nrn_nobanner_;
 
+extern void _hh_k_reg(void);
 extern void _pGPeA_mine_reg(void);
 
 void modl_reg(){
   if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
     fprintf(stderr, "Additional mechanisms from files\n");
 
+    fprintf(stderr," hh_k.mod");
     fprintf(stderr," pGPeA_mine.mod");
     fprintf(stderr, "\n");
   }
+  _hh_k_reg();
   _pGPeA_mine_reg();
 }
